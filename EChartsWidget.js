fccc5a1b8aa21e4da786b460460bdb0523be1faa
JH-cmyk <jeonghye@jadedata.net>
2025-09-01 13:52:04 +0900

    Update EChartsWidget.js

--- EChartsWidget.js
+++ EChartsWidget.js
@@ -1,39 +1,16 @@
 (function() { 
 	let template = document.createElement("template");
-	template.innerHTML = `
-		<style>
-		:host {
-			border-radius: 25px;
-			border-width: 4px;
-			border-color: black;
-			border-style: solid;
-			display: block;
-		} 
-		</style> 
-	`;
 
 	class ColoredBox extends HTMLElement {
 		constructor() {
 			super(); 
-			this.appendChild(template.content.cloneNode(true));
+            console.log("hello? 뭐임");
+			// this.appendChild(template.content.cloneNode(true));
 			// this.addEventListener("click", event => {
 			// 	var event = new Event("onClick");
 			// 	this.dispatchEvent(event);
 			// });
-			this._props = {};
-		}
-
-		onCustomWidgetBeforeUpdate(changedProperties) {
-			this._props = { ...this._props, ...changedProperties };
-		}
-
-		onCustomWidgetAfterUpdate(changedProperties) {
-			if ("color" in changedProperties) {
-				this.style["background-color"] = changedProperties["color"];
-			}
-			if ("opacity" in changedProperties) {
-				this.style["opacity"] = changedProperties["opacity"];
-			}
+			// this._props = {};
 		}
 	}
 
